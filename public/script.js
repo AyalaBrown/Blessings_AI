c5f9962a2fd65095d0f0b7340a8aa20b8d80bef4
Ayala Brown <Bayala@isrtech.co.il>
2024-01-21 12:17:36 +0200

    21/01/2024

--- public/script.js
+++ public/script.js
@@ -6,6 +6,26 @@ function sendData() {
   const atmosphere = document.getElementById('atmosphere').value;
   const type = document.getElementById('type').value;
   const age = document.getElementById('age').value;
+  const userSelectionsContainer = document.getElementById('userSelections');
+
+  let age_ = ''
+  if (age!== undefined && age!== ''){
+    age_ = ', Age - ' + age
+  }
+  userSelectionsContainer.innerText = `Selections: Event - ${event}, Atmosphere - ${atmosphere}, Type - ${type} ${age_}`;
+  userSelectionsContainer.style.display = 'block';
+
+  document.getElementById('newSelection').style.display = 'block';
+  document.getElementById('event').style.display = 'none';
+  document.getElementById('atmosphere').style.display = 'none';
+  document.getElementById('type').style.display = 'none';
+  document.getElementById('age').style.display = 'none';
+  document.getElementById('event_label').style.display = 'none';
+  document.getElementById('atmosphere_label').style.display = 'none';
+  document.getElementById('type_label').style.display = 'none';
+  document.getElementById('forAge').style.display = 'none';
+
+
 
   fetch('http://localhost:3000/processData', {
     method: 'POST',
@@ -28,8 +48,7 @@ function sendData() {
     // Enable the "I want another blessing" button
     document.getElementById('anotherButton').style.display = 'block';
     document.getElementById('result').style.display = 'block';
-    document.getElementById('generateButton').disabled = true; // Disable "Generate Blessing" button after initial click
-
+    document.getElementById('submitButton').style.display = 'none';
   })
   .catch(error => console.error('Error:', error));
 }
@@ -53,7 +72,7 @@ function showNextResult() {
     const event = document.getElementById('event');
     const ageInputDiv = document.getElementById('ageInput');
     const ageInput = document.getElementById('age');
-  
+
     if (event.value === 'birthday') {
       ageInputDiv.style.display = 'block';
     } else {
@@ -63,7 +82,22 @@ function showNextResult() {
     currentIndex = 1;
     document.getElementById('anotherButton').style.display = 'none';
     document.getElementById('result').style.display = 'none';
-    document.getElementById('generateButton').disabled = false;
+  }
+
+  function newSelection() {
+    document.getElementById('event').style.display = 'block';
+    document.getElementById('atmosphere').style.display = 'block';
+    document.getElementById('type').style.display = 'block';
+    document.getElementById('age').style.display = 'block';
+    document.getElementById('event_label').style.display = 'block';
+    document.getElementById('atmosphere_label').style.display = 'block';
+    document.getElementById('type_label').style.display = 'block';
+    document.getElementById('forAge').style.display = 'block';
+    document.getElementById('submitButton').style.display = 'block';
+    document.getElementById('userSelections').style.display = 'none';
+    document.getElementById('newSelection').style.display = 'none';
+    document.getElementById('anotherButton').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
   }
 
   
